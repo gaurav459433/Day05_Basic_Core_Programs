7f1601cb2061037f7da1735d866387484b6701f8
gaurav459433 <gaurav459433@gmail.com>
2023-02-25 14:02:36 +0530

    Added: Code For Print Power Of 2

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class BasicCorePrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The Year :");
-        int year = sc.nextInt();
-        leapYear(year);
+        System.out.println("Enter The Power For 2 :");
+        int n = sc.nextInt();
+        int power = 1;
+        powerOf2(n, power);
     }
 
-    //Program For Checking Leap Year OR Not
-    public static void leapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println("Given Year Is Leap Year");
-        } else {
-            System.out.println("Given Year Is Not Leap Year");
+    //Program For Power Of 2
+    public static void powerOf2(int n, int power) {
+        int i = 0;
+        if (0 <= n && n < 31) {  //input number should be less than 31 & greater than 0
+            while (i < n) {
+                power *= 2;  //for power of 2
+                i++;
+            }
         }
+        System.out.println("2^" + n + " is = " + " " + power);
+
     }
+
 }
