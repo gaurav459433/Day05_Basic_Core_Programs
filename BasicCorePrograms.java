cc014374da388ecb3c65f74a2b560b37db22bc03
gaurav459433 <gaurav459433@gmail.com>
2023-02-26 01:41:50 +0530

    Added: Code For Checking The Given Number Is Even OR Odd

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,25 +5,18 @@ import java.util.Scanner;
 public class BasicCorePrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the First Number: ");
-        int number1 = sc.nextInt();
-        System.out.println("Enter the Second Number: ");
-        int number2 = sc.nextInt();
-        int number = 0;
-        beforeSwap(number1, number2);
-
-        afterSwap(number1, number2, number);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+        checkEvenOdd(n);
     }
 
     //Program For Swap Two Numbers
-    public static void beforeSwap(int number1, int number2) {
-        System.out.println("Before Swapping: " + number1 + " " + number2 + " ");
-    }
-
-    public static void afterSwap(int number1, int number2, int number) {
-        number = number1;
-        number1 = number2;
-        number2 = number;
-        System.out.println("After Swapping: " + number1 + " " + number2 + " ");
+    public static void checkEvenOdd(int n) {
+        if (n % 2 == 0) {
+            System.out.println(n + " " +"is Even Number");
+        }
+        else {
+            System.out.println(n + " " +"is Odd Number");
+        }
     }
 }
