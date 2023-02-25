169fe2829f3a5137ad338d9a7d674e545891fdaf
gaurav459433 <gaurav459433@gmail.com>
2023-02-26 01:53:47 +0530

    Added: Code For Checking Given An Alphabet Is Vowel OR Consonant

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,18 +5,41 @@ import java.util.Scanner;
 public class BasicCorePrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number: ");
-        int n = sc.nextInt();
-        checkEvenOdd(n);
+        System.out.println("Enter an Alphabet: ");
+        char alphabet = sc.next().charAt(0);
+
+        checkVowelConsonant(alphabet);
     }
 
-    //Program For Swap Two Numbers
-    public static void checkEvenOdd(int n) {
-        if (n % 2 == 0) {
-            System.out.println(n + " " +"is Even Number");
+    //Program For Checking An Alphabet is Vowel Or Consonant
+    public static void checkVowelConsonant(char alphabet) {
+        switch (alphabet) {
+            case 'a':
+
+            case 'e':
+
+            case 'i':
+
+            case 'o':
+
+            case 'u':
+
+            case 'A':
+
+            case 'E':
+
+            case 'I':
+
+            case 'O':
+
+            case 'U':
+
+                System.out.println(alphabet + " is vowel");
+                break;
+
+            default:
+                System.out.println(alphabet + " is consonant");
         }
-        else {
-            System.out.println(n + " " +"is Odd Number");
-        }
+
     }
 }
