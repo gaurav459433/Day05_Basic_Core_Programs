c873d17aa319e89d011d3636e68248117edcfe62
gaurav459433 <gaurav459433@gmail.com>
2023-02-25 12:20:19 +0530

    Added: Code For Flip Coin & Print Percentage Of Heads & Tails

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -1,8 +1,33 @@
 package com.bridgelabz.assignmentsrfp259.day05.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class BasicCorePrograms {
     public static void main(String[] args) {
-        System.out.println("Welcome To Basic Core Programs");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number Of Times Want To Flip Coin :");
+        int numOfTimesToFlip = sc.nextInt();
+        double headsCount = 0;
+        double tailsCount = 0;
+        flipCoinHeadsTails(headsCount, tailsCount, numOfTimesToFlip);
     }
 
+    public static void flipCoinHeadsTails(double headsCount, double tailsCount, int numOfTimesToFlip) {
+        for (int i = 0; i < numOfTimesToFlip; i++) {
+            double coinFlip = Math.random();
+            if (coinFlip < 0.5) {
+                headsCount++;
+            } else {
+                tailsCount++;
+            }
+        }
+        System.out.println("Number Of Heads :" + headsCount);
+        System.out.println("Number Of Tails :" + tailsCount);
+
+        double headspercentage = headsCount / numOfTimesToFlip * 100;
+        double tailspercentage = tailsCount / numOfTimesToFlip * 100;
+
+        System.out.println("Percentage of Heads :" + headspercentage);
+        System.out.println("Percentage of tails :" + tailspercentage);
+    }
 }
