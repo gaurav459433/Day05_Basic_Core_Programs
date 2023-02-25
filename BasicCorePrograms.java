3f0cd8fc45a2c21e10126a7e33367a670ef182f4
gaurav459433 <gaurav459433@gmail.com>
2023-02-26 02:03:39 +0530

    Added: Code For Checking The Largest Among Three Numbers

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,41 +5,26 @@ import java.util.Scanner;
 public class BasicCorePrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter an Alphabet: ");
-        char alphabet = sc.next().charAt(0);
+        System.out.println("Enter the Three Numbers to find the Largest Number");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
 
-        checkVowelConsonant(alphabet);
+        largestNumber(num1, num2, num3);
     }
 
-    //Program For Checking An Alphabet is Vowel Or Consonant
-    public static void checkVowelConsonant(char alphabet) {
-        switch (alphabet) {
-            case 'a':
+    //Program For Checking the Largest Among Three Numbers
+    public static void largestNumber(int num1, int num2, int num3) {
+        int max;
 
-            case 'e':
-
-            case 'i':
-
-            case 'o':
-
-            case 'u':
-
-            case 'A':
-
-            case 'E':
-
-            case 'I':
-
-            case 'O':
-
-            case 'U':
-
-                System.out.println(alphabet + " is vowel");
-                break;
-
-            default:
-                System.out.println(alphabet + " is consonant");
+        if (num2 > num1 && num2 > num3) {
+            max = num2;
+        } else if (num3 > num1 && num3 > num2) {
+            max = num3;
+        } else {
+            max = num1;
         }
-
+        System.out.println("The Largest Number is: " + max);
     }
+
 }
