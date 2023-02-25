bf550573b7bbbb31fb19b5708edabe23ecea9b3c
gaurav459433 <gaurav459433@gmail.com>
2023-02-25 12:11:53 +0530

    Welcome To Basic Core Programs

---
+++ BasicCorePrograms.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.assignmentsrfp259.day05.basiccoreprograms;
+
+public class BasicCorePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Basic Core Programs");
+    }
+
+}
