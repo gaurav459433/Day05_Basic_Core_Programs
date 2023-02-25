3b4982c62f99a1dd0632d0990cda725566b83cbb
gaurav459433 <gaurav459433@gmail.com>
2023-02-25 13:04:42 +0530

    Added: Code For Cheking Leap Year OR Not

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,29 +5,17 @@ import java.util.Scanner;
 public class BasicCorePrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The Number Of Times Want To Flip Coin :");
-        int numOfTimesToFlip = sc.nextInt();
-        double headsCount = 0;
-        double tailsCount = 0;
-        flipCoinHeadsTails(headsCount, tailsCount, numOfTimesToFlip);
+        System.out.println("Enter The Year :");
+        int year = sc.nextInt();
+        leapYear(year);
     }
 
-    public static void flipCoinHeadsTails(double headsCount, double tailsCount, int numOfTimesToFlip) {
-        for (int i = 0; i < numOfTimesToFlip; i++) {
-            double coinFlip = Math.random();
-            if (coinFlip < 0.5) {
-                headsCount++;
-            } else {
-                tailsCount++;
-            }
+    //Program For Checking Leap Year OR Not
+    public static void leapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Given Year Is Leap Year");
+        } else {
+            System.out.println("Given Year Is Not Leap Year");
         }
-        System.out.println("Number Of Heads :" + headsCount);
-        System.out.println("Number Of Tails :" + tailsCount);
-
-        double headspercentage = headsCount / numOfTimesToFlip * 100;
-        double tailspercentage = tailsCount / numOfTimesToFlip * 100;
-
-        System.out.println("Percentage of Heads :" + headspercentage);
-        System.out.println("Percentage of tails :" + tailspercentage);
     }
 }
