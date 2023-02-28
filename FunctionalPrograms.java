62ec3a47706c7638cbdf467dfe1ab0b515c0913d
gaurav459433 <gaurav459433@gmail.com>
2023-03-01 00:45:52 +0530

    Added: Code For Sum Of 3 Integer Adds To Zero

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -4,30 +4,39 @@ import java.util.Scanner;
 
 public class FunctionalPrograms {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the values for m and n: ");
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        int arr[][] = new int[m][n];
-        takeInput(m, n, arr);
+        int [
+                ] arr = readInput();
+        int n = arr.length;
+        findTriplets(arr, n);
     }
 
-    static void takeInput(int m, int n, int[][] arr) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the values for the 2D arrays of dimensions: " + m + " x " + n);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = sc.nextInt();
+    static void findTriplets(int[] arr, int n) {
+        boolean found = false;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
+                        found = true;
+                    }
+                }
             }
         }
-        System.out.println("Array : ");
+        if (found == false)
+            System.out.println("Numbers with sum 0 are not exists");
 
-        //For Print Arrays
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+    }
+
+    public static int[] readInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Array length");
+        int arrLength = input.nextInt();
+        int[] arr = new int[arrLength];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < arrLength; i++) {
+            int numElement = input.nextInt();
+            arr[i] = numElement;
         }
+        return arr;
     }
 }
