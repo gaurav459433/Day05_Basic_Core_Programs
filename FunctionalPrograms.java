50164143dcf08c66bc60fe7048d29d7524b804a0
gaurav459433 <gaurav459433@gmail.com>
2023-03-01 01:33:57 +0530

    Added: Code For Wind Chill

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -5,26 +5,18 @@ import java.util.Scanner;
 public class FunctionalPrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a");
-        double a = sc.nextDouble();
-        System.out.println("Enter the value of b");
-        double b = sc.nextDouble();
-        System.out.println("Enter the value of c");
-        double c = sc.nextDouble();
-        roots(a, b, c);
+        System.out.println("Enter The Temperature : ");
+        double t = sc.nextDouble();
+        System.out.println("Enter The Wind speed : ");
+        double v = sc.nextDouble();
+        windChill(t, v);
     }
 
-    public static void roots(double a, double b, double c) {
-        double delta = b * b - 4 * a * c;
-        System.out.println("Delta is " + delta);
-        if (delta > 0) {
-            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-            System.out.println("Roots are:");
-            System.out.println("Roots of x1: " + x1);
-            System.out.println("Roots of x2: " + x2);
-        } else {
-            System.out.println("Delta is less than 0");
+    public static void windChill(double t, double v) {
+        double w = 35.74 + (0.6215 * t) + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
+
+        if ((t <= 50) && (v <= 120) && (v >= 3)) {
+            System.out.println("Wind Chill : " + w);
         }
     }
 }
