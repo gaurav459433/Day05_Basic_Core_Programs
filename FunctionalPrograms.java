e50925c5226b6d891b439f15bf16049e50b0a619
gaurav459433 <gaurav459433@gmail.com>
2023-03-01 01:14:46 +0530

    Added: Code For Roots Of Quadratic Equation

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -5,13 +5,26 @@ import java.util.Scanner;
 public class FunctionalPrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of x and y: ");
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        euclideanDistance(x,y);
+        System.out.println("Enter the value of a");
+        double a = sc.nextDouble();
+        System.out.println("Enter the value of b");
+        double b = sc.nextDouble();
+        System.out.println("Enter the value of c");
+        double c = sc.nextDouble();
+        roots(a, b, c);
     }
 
-    static void euclideanDistance(int x, int y) {
-        System.out.println(Math.sqrt((x * x) + (y * y)));
+    public static void roots(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+        System.out.println("Delta is " + delta);
+        if (delta > 0) {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Roots are:");
+            System.out.println("Roots of x1: " + x1);
+            System.out.println("Roots of x2: " + x2);
+        } else {
+            System.out.println("Delta is less than 0");
+        }
     }
 }
