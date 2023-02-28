987af7064b5f66c0a5773f7a08751cd2d48feb04
gaurav459433 <gaurav459433@gmail.com>
2023-03-01 00:54:56 +0530

    Added: Code For Euclidean Distance

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -4,39 +4,14 @@ import java.util.Scanner;
 
 public class FunctionalPrograms {
     public static void main(String[] args) {
-        int [
-                ] arr = readInput();
-        int n = arr.length;
-        findTriplets(arr, n);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x and y: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        euclideanDistance(x,y);
     }
 
-    static void findTriplets(int[] arr, int n) {
-        boolean found = false;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
-                        found = true;
-                    }
-                }
-            }
-        }
-        if (found == false)
-            System.out.println("Numbers with sum 0 are not exists");
-
-    }
-
-    public static int[] readInput() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the Array length");
-        int arrLength = input.nextInt();
-        int[] arr = new int[arrLength];
-        System.out.println("Enter the array elements: ");
-        for (int i = 0; i < arrLength; i++) {
-            int numElement = input.nextInt();
-            arr[i] = numElement;
-        }
-        return arr;
+    static void euclideanDistance(int x, int y) {
+        System.out.println(Math.sqrt((x * x) + (y * y)));
     }
 }
